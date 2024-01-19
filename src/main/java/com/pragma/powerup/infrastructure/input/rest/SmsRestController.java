182d7f9792e4b5c90ab0c3a272731779f8f8034c
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-19 18:10:08 -0500

    feat: new documentation for the sendSms endpoint within SsmRestController

--- src/main/java/com/pragma/powerup/infrastructure/input/rest/SmsRestController.java
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/SmsRestController.java
@@ -2,6 +2,10 @@ package com.pragma.powerup.infrastructure.input.rest;
 
 import com.pragma.powerup.application.dto.request.SmsInfoRequestDto;
 import com.pragma.powerup.application.handler.ISmsNotificationHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +21,19 @@ public class SmsRestController {
 
     private final ISmsNotificationHandler smsNotificationHandler;
 
+    @Operation(summary = "Send a message from the application to a specific client")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "The request was answered successfully",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Only an employee can send notifications to clients",
+                    content = @Content
+            )
+    })
     @PostMapping("/send")
     public ResponseEntity<Integer> sendSms(@RequestBody SmsInfoRequestDto smsInfoRequestDto){
 
