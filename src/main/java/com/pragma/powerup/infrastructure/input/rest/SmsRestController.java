9d2832e2283735e91d7c7bdc36d974565e8572d0
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-19 16:15:31 -0500

    feat: new SmsRestController within infrastructure

--- src/main/java/com/pragma/powerup/infrastructure/input/rest/SmsRestController.java
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/SmsRestController.java
@@ -1,2 +1,29 @@
-package com.pragma.powerup.infrastructure.input.rest;public class SmsRestController {
+package com.pragma.powerup.infrastructure.input.rest;
+
+import com.pragma.powerup.application.dto.request.SmsInfoRequestDto;
+import com.pragma.powerup.application.handler.ISmsNotificationHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sms/v1/")
+@RequiredArgsConstructor
+public class SmsRestController {
+
+    private final ISmsNotificationHandler smsNotificationHandler;
+
+    @PostMapping("/send")
+    public ResponseEntity<Integer> sendSms(@RequestBody SmsInfoRequestDto smsInfoRequestDto){
+
+         int securityCode = smsNotificationHandler.sendSms(smsInfoRequestDto);
+
+        return  ResponseEntity
+                .status(HttpStatus.OK)
+                .body(securityCode);
+    }
 }
